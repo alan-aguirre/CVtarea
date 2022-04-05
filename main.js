8ce48a5719062940eb5745e49591fe4534b034de
Alan Aguirre <a01657644@itesm.mx>
2022-04-05 16:18:16 -0500

    end

--- main.js
+++ main.js
@@ -60,7 +60,6 @@ const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
 const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
 
 function updateCountdown() {
-    // Calcs
     const NOW = new Date()
     const DURATION = DATE_TARGET - NOW;
     const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
@@ -68,16 +67,11 @@ function updateCountdown() {
     const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
     const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
 
-    // Render
     SPAN_DAYS.textContent = REMAINING_DAYS;
     SPAN_HOURS.textContent = REMAINING_HOURS;
     SPAN_MINUTES.textContent = REMAINING_MINUTES;
     SPAN_SECONDS.textContent = REMAINING_SECONDS;
 }
 
-//===
-// INIT
-//===
 updateCountdown();
-// Refresh every second
 setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
